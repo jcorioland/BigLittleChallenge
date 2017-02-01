521780d2fbcb7079fb99623764b6285ed79e9432
Julien Corioland <jucoriol@microsoft.com>
2017-02-01 14:44:25 -0800

    update swarmmode script

--- ci/run-swarmmode.sh
+++ ci/run-swarmmode.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+
+docker login -u $1 -p $2 $3 
+
 docker stack rm biglittlechallenge
-docker stack deploy -f docker-compose.yml
+docker stack deploy -c docker-compose.yml --with-registry-auth
