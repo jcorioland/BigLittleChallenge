18ef75c3d885e31a9238a90a16bbf9696639d73b
Julien Corioland <jucoriol@microsoft.com>
2017-02-02 16:42:52 -0800

    update deployment tools

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# login into the registry
 docker login -u $1 -p $2 $3 
 
+# deploy or upgrade the big little challenge stack
 docker stack deploy biglittlechallenge -c docker-compose.yml --with-registry-auth
