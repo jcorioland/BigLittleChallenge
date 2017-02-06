bb3e728715a3aea410888a5a5cd9b0e9bb9e6cd6
Julien Corioland <jcorioland@users.noreply.github.com>
2017-02-06 11:36:08 -0800

    update deploy script to remove old stack

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -3,5 +3,8 @@
 # login into the registry
 docker login -u $1 -p $2 $3 
 
+# remove the old stack
+docker stack rm biglittlechallenge
+
 # deploy or upgrade the big little challenge stack
 docker stack deploy biglittlechallenge -c docker-compose.yml --with-registry-auth
